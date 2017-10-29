bcbe3ffa1940a4c994256aaece80bd708a893721
Nadyap <nadyaprabaningrum@gmail.com>
2017-10-29 18:53:12 +0700

    closing prepared stmt

--- Jogmedia/src/main/java/com/Jogmedia/Jogmedia/dao/My_Connection.java
+++ Jogmedia/src/main/java/com/Jogmedia/Jogmedia/dao/My_Connection.java
@@ -2,6 +2,8 @@ package com.Jogmedia.Jogmedia.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class My_Connection {
     protected Connection con;
@@ -33,6 +35,19 @@ public class My_Connection {
         catch (Exception e){
             System.out.println("Error while disconnect db");
             System.out.println(e);
+        }
+    }
+    public static void ClosePreparedStatement(PreparedStatement preparedStatement){
+        if(preparedStatement==null){
+            return;
+        } else{
+            try {
+                preparedStatement.close();
+                System.out.println("Success closing prepared statement");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Error while closing prepared statement : " + e.toString());
+            }
         }
     }
 }
