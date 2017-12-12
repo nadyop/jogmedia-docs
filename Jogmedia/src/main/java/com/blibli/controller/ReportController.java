e5a739bfc800b89c9988c97ae39842cd054c617c
Nadya Prabaningrum <nadya.prabaningrum@mail.ugm.ac.id>
2017-12-12 16:35:20 +0700

    add report page

---
+++ Jogmedia/src/main/java/com/blibli/controller/ReportController.java
@@ -0,0 +1,12 @@
+package com.blibli.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReportController {
+    @RequestMapping("/report")
+    public String report(){
+        return "report";
+    }
+}
